e3c2c5e887d24d0cb01612a5e86bd274222c2212
João Felipe Kraemer Silveira <jo_oaoo@hotmail.com>
2023-09-28 14:51:28 -0300

    feat: model client

---
+++ backend/src/database/models/client.ts
@@ -0,0 +1,41 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class Client extends Model {
+    declare idClient: number;
+    declare email: string;
+    declare role: string;
+    declare passwordHash: string;
+    declare name: string;
+}
+
+Client.init({
+    idClient: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: STRING,
+      allowNull: false,
+    },
+    role: {
+        type: STRING,
+        allowNull: false,
+    },
+    passwordHash: {
+        type: STRING,
+        allowNull: false,
+        field: 'password_hash',
+    },
+    name: {
+        type: STRING,
+        allowNull: false
+    },
+}, {
+    sequelize: db,
+    modelName: 'client',
+    underscored: true,
+    timestamps: false,
+  })
